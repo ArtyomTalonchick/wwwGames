97be437af10ddd329d2d96e67372252029920764
Артем Таланец <artyom081099@mail.ru>
2019-12-08 13:44:50 +0300

    Created tagHelper and used it

---
+++ wwwGames/TagHelpers/TeamNameTagHelper .cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Linq;
+
+namespace wwwGames.TagHelpers
+{
+    public class TeamNameTagHelper : TagHelper
+    {
+        public string id { get; set; }
+        ContextDb db;
+
+        public TeamNameTagHelper(ContextDb context)
+        {
+            db = context;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";
+            output.Attributes.SetAttribute("class", "large");
+            output.Content.SetContent(db.Teams.FirstOrDefault(t => t.Id.ToString() == id).Name);
+        }
+    }
+}
