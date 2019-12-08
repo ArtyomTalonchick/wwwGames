ab831bc7cc2500c94e15a0167876de50ad4d11c1
Артем Таланец <artyom081099@mail.ru>
2019-12-08 15:15:59 +0300

    Created ViewComponent and used it

---
+++ wwwGames/Components/TeamCard.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using wwwGames.Models;
+
+namespace wwwGames.Components
+{
+    public class TeamCard : ViewComponent
+    {
+        public IViewComponentResult Invoke(Team team)
+        {
+            return View("TeamCard", team);
+        }
+    }
+}
